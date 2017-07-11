472fa4a53780563a2cc86c662a12547a19b3a8fc
samu <samu.ampio@student.tut.fi>
2017-07-11 15:37:42 +0300

    Added dummy functionality to load and aoa bars.

--- visual_interface.py
+++ visual_interface.py
@@ -1,7 +1,7 @@
 """This module contains the visul interface window that displays
 all the gathered and plotted data from the simulator"""
 
-from tkinter import Tk, Label, Frame, Canvas, PhotoImage
+from tkinter import Tk, Label, Frame, Canvas
 from udp_receiver import UDPReceiver
 
 
@@ -12,29 +12,26 @@ class EnvelopeWindow:
 
         # Initializing window
         self.__root = Tk()
-        self.__root.title("Envelope")
+        self.__root.title("Simulator Envelope")
         self.__root.resizable(width=False, height=False)
         # self.__root.geometry('{}x{}'.format(1600, 800))
 
         Label(self.__root, text="Value log").grid(row=1, column=1,
                                                   columnspan=4)
-        Label(self.__root, text="AOA bar", justify="left").grid(row=4,
-                                                                column=4,
-                                                                sticky="w")
-        Label(self.__root, text="Load bar", justify="right").grid(row=4,
-                                                                column=1,
-                                                                sticky="e")
+        Label(self.__root, text="AOA bar", justify="left")\
+            .grid(row=4, column=4, sticky="w")
+        Label(self.__root, text="Load bar", justify="right")\
+            .grid(row=4, column=1, sticky="e")
         Label(self.__root, text="Envelope plot", justify="right")\
             .grid(row=1, column=5, sticky="s")
         Label(self.__root, text="Load plot", justify="right") \
             .grid(row=1, column=6, sticky="s")
-        Label(self.__root, text="Load bar", justify="right").grid(row=4,
-                                                                  column=1,
-                                                                  sticky="e")
+        Label(self.__root, text="Load bar", justify="right")\
+            .grid(row=4, column=1, sticky="e")
         Label(self.__root, height=0).grid(row=3, column=1, columnspan=4)
         Label(self.__root, height=0).grid(row=5, column=1, columnspan=4)
-        Label(self.__root, text="Visual inclination").grid(row=6, column=1,
-                                                           columnspan=4)
+        Label(self.__root, text="Absolute inclination").grid(row=6, column=1,
+                                                             columnspan=4)
 
         self.__logframe = Frame(self.__root, borderwidth=4, relief="sunken")
         self.__logframe.grid(row=2, column=1, columnspan=4, sticky="n")
@@ -72,27 +69,27 @@ class EnvelopeWindow:
         self.__oldpitch = None
         self.__oldroll = None
         self.__oldaoa = None
-        self.__oldinclcoords = [0, 0, 0, 0, 0, 0, 0, 0]
 
         self.__aoaframe = Canvas(self.__root, width=25, height=300,
                                  borderwidth=4,
                                  relief="sunken", bg="white")
         self.__aoaframe.grid(row=4, column=3, sticky="nw")
-        aoabar = PhotoImage(file="images/aoa.gif")
-        self.__aoaframe.create_image(40, 150, image=aoabar, tags="bar")
-        self.__aoaframe.create_line(0, 150, 78, 150, fill="white")
+        self.__aoaframe.create_line(0, 150, 30, 150, fill="black")
+        self.__aoaframe.create_polygon(0, 150, 30, 150, 30, 150, 0, 150,
+                                       fill="#005a08", tags="aoabar")
 
         self.__loadframe = Canvas(self.__root, width=25, height=300,
-                                 borderwidth=4,
-                                 relief="sunken", bg="white")
+                                  borderwidth=4,
+                                  relief="sunken", bg="white")
         self.__loadframe.grid(row=4, column=2, sticky="ne")
+        self.__loadframe.create_line(0, 150, 30, 150, fill="black")
+        self.__loadframe.create_polygon(0, 150, 30, 150, 30, 150, 0, 150,
+                                        fill="#005a08", tags="loadbar")
 
-        # skybox = PhotoImage("images/skybox.gif")
         self.__inclframe = Canvas(self.__root, width=200, height=200,
                                   borderwidth=4,
                                   relief="sunken", bg="#9ea9fe")
         self.__inclframe.grid(row=7, column=1, columnspan=4, sticky="n")
-        # self.__inclframe.create_image(40, 150, image=skybox, tags="box")
         self.__inclframe.create_line(30, 100, 90, 100, fill="black", width=2)
         self.__inclframe.create_line(90, 100, 90, 110, fill="black", width=2)
         self.__inclframe.create_line(110, 100, 110, 110, fill="black", width=2)
@@ -105,8 +102,8 @@ class EnvelopeWindow:
 
         self.__rx = UDPReceiver()
         self.__log = open("capture2.txt", "r")
-        # self.__root.after(100, self.read_log)
-        self.__root.after(100, self.listen_udp)
+        self.__root.after(100, self.read_log)
+        # self.__root.after(100, self.listen_udp)
 
         self.__root.mainloop()
 
@@ -139,14 +136,16 @@ class EnvelopeWindow:
     def display_data(self, packet):
         """DOCSTRING"""  # TODO Docstring
         import time
-        t1 = float(time.time())
+        t0 = float(time.time())
         self.update_logframe(packet)
         self.update_plotframe(packet)
         self.update_aoaframe(packet)
         self.update_inclframe(packet)
+        self.update_loadframe(packet)
         self.__oldpitch = packet["PTC"]
         self.__oldroll = packet["ROL"]
-        print(float(time.time()) - t1)
+        self.__oldaoa = packet["PTC"]
+        print(float(time.time()) - t0)
         return
 
     def update_logframe(self, packet):
@@ -166,10 +165,11 @@ class EnvelopeWindow:
         scale = 5
         try:
             self.__datapoints \
-                .append(self.__plotframe.create_line(self.__oldroll*scale+300,
-                                                     self.__oldpitch*-scale+300,
-                                                     packet["ROL"]*scale+300,
-                                                     packet["PTC"]*-scale+300))
+                .append(self.__plotframe
+                        .create_line(self.__oldroll*scale+300,
+                                     self.__oldpitch*-scale+300,
+                                     packet["ROL"]*scale+300,
+                                     packet["PTC"]*-scale+300))
             dp = (packet["PTC"] - self.__oldpitch) * -scale
             dr = (packet["ROL"] - self.__oldroll) * scale
         except TypeError:
@@ -183,19 +183,34 @@ class EnvelopeWindow:
 
     def update_aoaframe(self, packet):
         """DOCSTRING"""  # TODO Docstring
-        try:
-            dy = (packet["PTC"] - self.__oldaoa) * 6
-        except TypeError:
-            dy = packet["PTC"] * 6
-        self.__oldaoa = packet["PTC"]
-        self.__aoaframe.move("bar", 0, dy)
+        self.__aoaframe.coords(self.__aoaframe.find_withtag("aoabar"),
+                               [0, 150, 30, 150, 30, 150-(3*packet["PTC"]), 0,
+                                150-(3*packet["PTC"])])
+        color = hex(int(packet["PTC"]/90*255*2)).lstrip('0')
+        color = color.lstrip('x')
+        if len(color) == 1:
+            color = '0'+color
+        self.__aoaframe.itemconfig("aoabar", fill="#{}5a08".format(color))
+        return
+
+    def update_loadframe(self, packet):
+        """DOCSTRING"""  # TODO Docstring
+        self.__loadframe.coords(self.__loadframe.find_withtag("loadbar"),
+                                [0, 150, 30, 150, 30, 150-(6*packet["PTC"]), 0,
+                                150-(6*packet["PTC"])])
+        color = hex(int(packet["PTC"]/90*255*4)).lstrip('0')
+        color = color.lstrip('x')
+        if len(color) == 1:
+            color = '0'+color
+        self.__loadframe.itemconfig("loadbar", fill="#{}5a08".format(color))
+        return
 
     def update_inclframe(self, packet):
         """DOCSTRING"""  # TODO Docstring
         try:
-            dy = (packet["PTC"] - self.__oldpitch) * 3
+            dy = (packet["PTC"] - self.__oldpitch) * 2
         except TypeError:
-            dy = packet["PTC"] * 3
+            dy = packet["PTC"] * 2
         newxy = []
         i = 0
         for x, y in self.__gndxy:
@@ -209,11 +224,14 @@ class EnvelopeWindow:
         deg = packet["ROL"]
         rad = (math.pi / 180) * deg
         newxy = []
+        i = 0
         for x, y in self.__gndxy:
             v = complex(math.cos(-rad), math.sin(-rad)) * \
                 (complex(x, y) - complex(100, 100)) + complex(100, 100)
             newxy.append(v.real)
             newxy.append(v.imag)
+            self.__gndxy[i] = (x, y + dy)
+            i += 1
         self.__inclframe.coords(self.__inclframe.find_withtag("gnd"), *newxy)
 
 if __name__ == '__main__':
