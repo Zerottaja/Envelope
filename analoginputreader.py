823db5da6957d0154963da4e20a5b8395d17b5a9
samu <samu.ampio@finaa.fi>
2017-08-03 13:35:15 +0300

    Added analog input reader module.

---
+++ analoginputreader.py
@@ -0,0 +1,37 @@
+"""This module contains a class AnalogInputReader that reads
+aircraft control values from a RPi analog shield and returns them."""
+
+from gpiozero import MCP3008
+
+
+class AnalogInputReader:
+    """AnalogInputReader reads the voltages of a RPi analog shield inputs and
+    transforms the voltages into control data."""
+
+    def __init__(self):
+        self.ele = MCP3008(channel=1)
+        self.ail = MCP3008(channel=2)
+        self.rud = MCP3008(channel=3)
+
+    def read_analog(self):
+        """read_analog() gets the values of the input pins
+        and packs them in a dictionary."""
+        packet = dict()
+        packet["ELE"] = self.ele.value
+        packet["AIL"] = self.ail.value
+        packet["RUD"] = self.rud.value
+        packet = self.formatter(packet)
+        return packet
+
+    @staticmethod
+    def formatter(packet):
+        """formatter() transforms the input voltages into control data."""
+
+        return packet
+
+
+if __name__ == '__main__':
+    AIR = AnalogInputReader()
+    AIR_DATA = AIR.read_analog()
+    if AIR_DATA:
+        print(AIR_DATA)
