e55c77f0b33be42b512ddcc65097813d96f16416
samu <samu.ampio@finaa.fi>
2017-08-09 12:03:57 +0300

    Changed shell script to launch relay instead of visual interface.

---
+++ launch_relay.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+clear
+echo "Looking for a FIFO-pipe..."
+if [ ! -p packet_fifo ]
+then
+    echo Making a FIFO pipe...
+    mkfifo packet_fifo
+    echo Done!
+fi
+echo Launching visual interface...
+python3 relay_node.py &
+sleep 1
+echo Done!
+echo Launching TCP-packet capturing...
+tshark -i enp0s25 -Y "ip.src == 192.9.200.155 and tcp.len == 1024 and (data.data[2] == 11)" -Eheader=n -Tfields -e data.data > packet_fifo
+echo Exiting...
