16ada9e791c1b50ccc793823234302690a360e82
Samu <samu.ampio@student.tut.fi>
2017-08-10 12:50:32 +0000

    Removed HexDumpReader from relay.

--- relay_node.py
+++ relay_node.py
@@ -2,7 +2,6 @@
 and packs it into a csv string."""
 
 from time import sleep
-from hexdumpreader import HexDumpReader
 from analoginputreader import AnalogInputReader
 
 
@@ -11,25 +10,15 @@ class RelayNode:
     which are read regularly."""
 
     def __init__(self):
-        self.HDR = HexDumpReader()
         self.AIR = AnalogInputReader()
-        self.__old_packet = {"ROL": 0.0, "PTC": 0.0, "HDG": 0.0, "AOA": 0.0,
-                             "SDS": 0.0, "LOA": 0.0, "ASP": 0.0, "FLP": 0.0}
         return
 
     def gather_data(self):
         """gather_data() calls reader objects' reading methods
         and packs their data together."""
 
-        try:
-            tcp_packet = self.HDR.read_hexdump()
-            self.__old_packet = tcp_packet
-        except TimeoutError:
-            tcp_packet = self.__old_packet
         analog_packet = self.AIR.read_analog()
-        for header in tcp_packet:
-            analog_packet[header] = tcp_packet[header]
-        # packet = {**tcp_packet, **analog_packet}
+
         csv_string = self.formatter(analog_packet)
 
         return csv_string
@@ -40,9 +29,9 @@ class RelayNode:
         into csv strings in a predefined order."""
 
         csv_string = ""
-        for header in ("ROL", "PTC", "HDG", "AOA", "SDS", "ASP",
-                       "LOA", "ELE", "AIL", "RUD", "FLP"):
-            csv_string += str(packet[header])
+        for header in ("ELE", "AIL", "RUD"):
+            csv_string += str(packet[header]) + ","
+        csv_string = csv_string[:-1]
 
         return csv_string
 
